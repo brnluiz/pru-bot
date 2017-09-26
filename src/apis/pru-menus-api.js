7d103c3cbd6ced051a3ce4182b6ad645abf3bc89
Bruno Luiz <contact@brunoluiz.net>
2017-09-25 23:53:15 -0300

    Implement an api connector for pru-menus

---
+++ src/apis/pru-menus-api.js
@@ -0,0 +1,39 @@
+const moment = require('moment')
+const request = require('axios')
+
+const configs = {
+  baseURL: process.env.PRU_MENUS_URI,
+  auth: {
+    username: process.env.PRU_MENUS_AUTH_USER,
+    password: process.env.PRU_MENUS_AUTH_PASSWORD
+  }
+}
+
+module.exports = {
+  getWeek (locationId) {
+    const week = moment().startOf('isoweek')
+
+    const startDate = week.format('YYYY-MM-DD')
+    const endDate = week.add(6, 'days').format('YYYY-MM-DD')
+
+    return request.get(`/locations/${locationId}/menus` +
+      `?startDate=${startDate}&endDate=${endDate}`, configs)
+  },
+  getToday (locationId) {
+    const today = moment()
+      .startOf('day')
+      .format('YYYY-MM-DD')
+
+    return request
+      .get(`/locations/${locationId}/menus?date=${today}`, configs)
+  },
+  getTomorrow (locationId) {
+    const tomorrow = moment()
+      .startOf('day')
+      .add(1, 'day')
+      .format('YYYY-MM-DD')
+
+    return request
+      .get(`/v1/locations/${locationId}/menus?date=${tomorrow}`, configs)
+  }
+}

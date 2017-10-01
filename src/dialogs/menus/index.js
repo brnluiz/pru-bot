92da2768094236dc47c95de086215e82435b347d
Bruno Luiz <contact@brunoluiz.net>
2017-10-01 01:15:05 -0300

    Add today-menu and re-organizes menu index

--- src/dialogs/menus/index.js
+++ src/dialogs/menus/index.js
@@ -1,11 +1,17 @@
-const weekMenuDialog = require('./week-menu-dialog')
 const menuDialog = require('./menu-dialog')
+const todayMenuDialog = require('./today-menu-dialog')
+const weekMenuDialog = require('./week-menu-dialog')
 
 module.exports = (bot) => {
+  bot.dialog('MenuDialog', menuDialog)
+
+  bot.dialog('TodayMenuDialog', todayMenuDialog)
+    .triggerAction({ matches: /^(today)/i })
+
   bot.dialog('WeekMenuDialog', weekMenuDialog)
     .triggerAction({ matches: /^(week)/i })
-  bot.beginDialogAction('WeekMenuAction', 'WeekMenuDialog')
 
-  bot.dialog('MenuDialog', menuDialog)
   bot.beginDialogAction('OpenMenuAction', 'MenuDialog')
+  bot.beginDialogAction('TodayMenuAction', 'TodayMenuDialog')
+  bot.beginDialogAction('WeekMenuAction', 'WeekMenuDialog')
 }

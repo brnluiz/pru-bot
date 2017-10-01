2194ef812de6921ee7e228417e4ef6cac7c4c691
Bruno Luiz <contact@brunoluiz.net>
2017-10-01 01:08:36 -0300

    Add the WeekMenuAction to menus

--- src/dialogs/menus/index.js
+++ src/dialogs/menus/index.js
@@ -4,6 +4,7 @@ const menuDialog = require('./menu-dialog')
 module.exports = (bot) => {
   bot.dialog('WeekMenuDialog', weekMenuDialog)
     .triggerAction({ matches: /^(week)/i })
+  bot.beginDialogAction('WeekMenuAction', 'WeekMenuDialog')
 
   bot.dialog('MenuDialog', menuDialog)
   bot.beginDialogAction('OpenMenuAction', 'MenuDialog')

da102224761970293543f8930d2bca17db298db3
Bruno Luiz <contact@brunoluiz.net>
2017-10-01 01:14:52 -0300

    Add today-menu-dialog

---
+++ src/dialogs/menus/today-menu-dialog.js
@@ -0,0 +1,16 @@
+const menuService = require('../../services/menu-service')
+
+module.exports = [
+  async (session, results, next) => {
+    // TODO: This should not be hardcoded!
+    const menus = await menuService.getToday('ufsc-trindade')
+
+    if (!menus.length) {
+      session.send('menus:notavailable')
+    }
+
+    const payload = JSON.stringify(menus[0])
+
+    return session.beginDialog('MenuDialog', payload)
+  }
+]

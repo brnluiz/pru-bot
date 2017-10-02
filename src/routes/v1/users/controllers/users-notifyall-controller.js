a8f41dd2846b3f448cd69b05eae938838a3f8f41
Bruno Luiz <contact@brunoluiz.net>
2017-10-01 22:33:15 -0300

    Check if the today menu exists before notifying the users

--- src/routes/v1/users/controllers/users-notifyall-controller.js
+++ src/routes/v1/users/controllers/users-notifyall-controller.js
@@ -1,9 +1,19 @@
-const userService = require('../../../../services/user-service')
 const { bot } = require('../../../../dialogs')()
+const errors = require('../../../../errors')
+const menuService = require('../../../../services/menu-service')
+const userService = require('../../../../services/user-service')
 
 module.exports = async (req, res, next) => {
-  const users = await userService.getAll()
+  // TODO: This should not be hardcoded!
+  const todayMenu = await menuService.getToday('ufsc-trindade')
+  if (!todayMenu.length) {
+    return next(new errors.NotFoundError('No Menu Found for Today'))
+  }
 
+  // TODO: You should be asking yourself: what if pru-bot has 1,000,000 users?
+  // The answer is: yes, you thought right, this doesn't scale. Actually, it should
+  // be put on a queue (but no time for this right now)
+  const users = await userService.getAll()
   users.forEach(user =>
     bot.beginDialog(user.address, 'TodayMenuDialog'))
 }

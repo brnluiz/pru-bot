9d7e9579672a320586bbd3293de32965fb1a34b8
Bruno Luiz <contact@brunoluiz.net>
2017-10-01 23:22:00 -0300

    Add error handling to today and week menu

--- src/dialogs/menus/today-menu-dialog.js
+++ src/dialogs/menus/today-menu-dialog.js
@@ -1,16 +1,25 @@
 const menuService = require('../../services/menu-service')
 
+const configs = require('../../../configs')
+const log = require('../../log')
+
 module.exports = [
   async (session, results, next) => {
-    // TODO: This should not be hardcoded!
-    const menus = await menuService.getToday('ufsc-trindade')
+    try {
+      // TODO: This should not be hardcoded!
+      const menus = await menuService
+        .getToday(configs.general.defaults.location)
 
-    if (!menus.length) {
-      session.send('menus:notavailable')
+      if (!menus.length) {
+        session.send('menus:notavailable')
+      }
+
+      const payload = JSON.stringify(menus[0])
+
+      return session.replaceDialog('MenuDialog', payload)
+    } catch (err) {
+      log.error({ err }, 'Error on TodayMenuDialog')
+      return session.endDialog('menus:notavailable')
     }
-
-    const payload = JSON.stringify(menus[0])
-
-    return session.replaceDialog('MenuDialog', payload)
   }
 ]
--- src/dialogs/menus/week-menu-dialog.js
+++ src/dialogs/menus/week-menu-dialog.js
@@ -2,47 +2,54 @@ const builder = require('botbuilder')
 const moment = require('moment')
 
 const configs = require('../../../configs')
+const log = require('../../log')
 const menuService = require('../../services/menu-service')
 const menuImagesHelper = require('./helpers/menu-images-helper')
 
 module.exports = [
   async (session, results, next) => {
-    session.sendTyping()
+    try {
+      session.sendTyping()
 
-    const images = menuImagesHelper.generate()
+      const images = menuImagesHelper.generate()
 
-    // TODO: This should not be hardcoded!
-    const menus = await menuService.getWeek('ufsc-trindade')
-    if (!menus.length) {
-      return session.endDialog('menu:notavailable')
+      // TODO: This should not be hardcoded!
+      const menus = await menuService
+        .getWeek(configs.general.defaults.location)
+      if (!menus.length) {
+        return session.endDialog('menus:notavailable')
+      }
+
+      const cards = menus.map((item, index) => {
+        const payload = JSON.stringify(item)
+
+        const date = moment(item.date).utc()
+        const dateNumber = date.format('DD/M/YY')
+        const dateString = date.locale(configs.general.defaults.locale)
+          .format('dddd')
+        const title = `${dateString} (${dateNumber})`
+
+        const button = builder.CardAction
+          .dialogAction(session, 'OpenMenuAction', payload, 'menus:view')
+
+        const card = new builder.HeroCard(session)
+          .title(title)
+          .images([builder.CardImage.create(session, images[index])])
+          .buttons([button])
+          .tap(button)
+
+        return card
+      })
+
+      const carousel = new builder.Message(session)
+          .textFormat(builder.TextFormat.xml)
+          .attachmentLayout(builder.AttachmentLayout.carousel)
+          .attachments(cards)
+
+      return session.endDialog(carousel)
+    } catch (err) {
+      log.error({ err }, 'Error on WeekMenuDialog')
+      return session.endDialog('menus:notavailable')
     }
-
-    const cards = menus.map((item, index) => {
-      const payload = JSON.stringify(item)
-
-      const date = moment(item.date).utc()
-      const dateNumber = date.format('DD/M/YY')
-      const dateString = date.locale(configs.general.defaults.locale)
-        .format('dddd')
-      const title = `${dateString} (${dateNumber})`
-
-      const button = builder.CardAction
-        .dialogAction(session, 'OpenMenuAction', payload, 'menus:view')
-
-      const card = new builder.HeroCard(session)
-        .title(title)
-        .images([builder.CardImage.create(session, images[index])])
-        .buttons([button])
-        .tap(button)
-
-      return card
-    })
-
-    const carousel = new builder.Message(session)
-        .textFormat(builder.TextFormat.xml)
-        .attachmentLayout(builder.AttachmentLayout.carousel)
-        .attachments(cards)
-
-    return session.endDialog(carousel)
   }
 ]

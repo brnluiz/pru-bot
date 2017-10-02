f118dcdb6642de392e30fb53c7ad80c581d1175f
Bruno Luiz <contact@brunoluiz.net>
2017-10-01 22:50:00 -0300

    Update joi user-schema

--- src/db/schemas/user-schema.js
+++ src/db/schemas/user-schema.js
@@ -1,8 +1,9 @@
 const Joi = require('joi')
 
 module.exports = Joi.object().keys({
+  _id: Joi.string().required(),
   address: Joi.object().required(),
-  user: Joi.object().required(),
+  subscribed: Joi.boolean(),
   updatedAt: Joi.date().forbidden(),
   createdAt: Joi.date().forbidden()
 })

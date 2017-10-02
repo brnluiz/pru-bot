1fdd8320ac77985861496bafb4920dc34872d448
Bruno Luiz <contact@brunoluiz.net>
2017-10-01 23:49:43 -0300

    Add documentation on users-notify-all route

--- src/routes/v1/users/routes.js
+++ src/routes/v1/users/routes.js
@@ -2,6 +2,15 @@ const router = require('express').Router()
 
 const usersNotifyAllController = require('./controllers/users-notifyall-controller')
 
+/**
+ * @api {post} /users/notifyAll Notify all available users
+ * @apiGroup Users
+ * @apiVersion 1.0.0
+ *
+ * @apiSuccess Success
+ * @apiUse NotFoundError
+ * @apiUse InternalServerError
+ */
 router.post('/users/notifyAll', usersNotifyAllController)
 
 module.exports = router

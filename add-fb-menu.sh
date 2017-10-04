8d54c0eee199fde23b3ae493f992cf8151b9bd2a
Bruno Luiz <contact@brunoluiz.net>
2017-10-03 23:48:27 -0300

    Add menu-box to facebook bot

---
+++ add-fb-menu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [ -n "$1" ]; then
+  pagetoken=$1
+  echo "Page token: $pagetoken"
+else
+  echo "You need to pass the page token as parameter"
+  exit
+fi
+
+
+curl -X DELETE -H "Content-Type: application/json" -d '{
+  "setting_type":"call_to_actions",
+  "thread_state":"existing_thread"
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=$pagetoken"
+
+curl -X POST -H "Content-Type: application/json" -d '{
+  "setting_type" : "call_to_actions",
+  "thread_state" : "existing_thread",
+  "call_to_actions":[
+    {
+      "type":"postback",
+      "title":"Configurar Alertas",
+      "payload":"action?SubscriptionsAction"
+    },
+    {
+      "type": "postback",
+      "title":"Ver Cardápio",
+      "payload":"action?WeekMenuAction"
+    },
+    {
+      "type":"postback",
+      "title":"Ver Cardápio de Hoje",
+      "payload":"action?TodayMenuAction"
+    }
+  ]
+}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=$pagetoken"
+

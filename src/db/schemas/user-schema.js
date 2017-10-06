13dd217409a3f6bb981a7262f2350d391ae335cd
Bruno Luiz da Silva <contato@brunoluiz.net>
2017-10-06 16:23:06 -0300

    Remove the _id validation on the user joi schema (mongoose will do that)

--- src/db/schemas/user-schema.js
+++ src/db/schemas/user-schema.js
@@ -1,7 +1,6 @@
 const Joi = require('joi')
 
 module.exports = Joi.object().keys({
-  _id: Joi.string().required(),
   address: Joi.object().required(),
   subscribed: Joi.boolean(),
   updatedAt: Joi.date().forbidden(),

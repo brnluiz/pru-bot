b7cfaf1dbcf7bfcbf43216b9d9cf30839e127596
Bruno Luiz <contact@brunoluiz.net>
2017-10-08 16:47:50 -0300

    Add redis configs

--- configs/apis.js
+++ configs/apis.js
@@ -1,9 +1,12 @@
+const cache = require('./cache')
+
 module.exports = {
   menus: {
-    baseURL: process.env.PRU_MENUS_URI,
     auth: {
       username: process.env.PRU_MENUS_AUTH_USER,
       password: process.env.PRU_MENUS_AUTH_PASSWORD
-    }
+    },
+    baseURL: process.env.PRU_MENUS_URI,
+    cache
   }
 }
---
+++ configs/cache.js
@@ -0,0 +1,5 @@
+module.exports = {
+  redisUrl: process.env.REDIS_URL,
+  defaultExpiration: (60 * 60 * 6),
+  verbose: true
+}

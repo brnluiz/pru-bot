c692a3c8a4a320ccf17aa40f3d33ce29bafa1555
Bruno Luiz <contact@brunoluiz.net>
2017-10-09 21:26:50 -0300

    Fix HelpAction issue

--- src/dialogs/main/index.js
+++ src/dialogs/main/index.js
@@ -4,5 +4,5 @@ module.exports = (bot) => {
   bot.dialog('/main', mainDialog)
     .triggerAction({ matches: /^(help|ajuda|como)/i })
 
-  bot.beginDialogAction('HelpAction', '/help')
+  bot.beginDialogAction('HelpAction', '/main')
 }

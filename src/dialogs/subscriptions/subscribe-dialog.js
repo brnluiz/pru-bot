d006e751b4eaca234d781f2cb85d0f02b489d383
Bruno Luiz <contact@brunoluiz.net>
2017-10-24 22:08:56 -0200

    Localized buttons for subscribe and unsubscribe dialogs

--- src/dialogs/subscriptions/subscribe-dialog.js
+++ src/dialogs/subscriptions/subscribe-dialog.js
@@ -3,17 +3,22 @@ const builder = require('botbuilder')
 const log = require('../../log')
 const userService = require('../../services/user-service')
 
+const locale = {}
 const isResponseYes = results =>
-  (results.response && results.response.entity === 'yes')
+  (results.response && results.response.entity === locale.confirmYes)
 
 module.exports = [
-  (session) => {
+  (session, results, next) => {
+    locale.confirmYes = session.localizer.gettext('pt', 'confirm-yes')
+    locale.confirmNo = session.localizer.gettext('pt', 'confirm-no')
+
     builder.Prompts.choice(session, 'subscriptions:subscribe-prompt',
-      'yes|no', {
+      [locale.confirmYes, locale.confirmNo], {
         maxRetries: 0,
         promptAfterAction: false
       })
-  }, (session, results, next) => {
+  },
+  (session, results, next) => {
     if (isResponseYes(results)) return next()
 
     return session.endDialog('subscriptions:not-subscribed')
--- src/dialogs/subscriptions/unsubscribe-dialog.js
+++ src/dialogs/subscriptions/unsubscribe-dialog.js
@@ -3,13 +3,17 @@ const builder = require('botbuilder')
 const log = require('../../log')
 const userService = require('../../services/user-service')
 
+const locale = {}
 const isResponseYes = results =>
-  (results.response && results.response.entity === 'yes')
+  (results.response && results.response.entity === locale.confirmYes)
 
 module.exports = [
-  (session) => {
+  (session, results, next) => {
+    locale.confirmYes = session.localizer.gettext('pt', 'confirm-yes')
+    locale.confirmNo = session.localizer.gettext('pt', 'confirm-no')
+
     builder.Prompts.choice(session, 'subscriptions:unsubscribe-prompt',
-      'yes|no', {
+      [locale.confirmYes, locale.confirmNo], {
         maxRetries: 0,
         promptAfterAction: false
       })
